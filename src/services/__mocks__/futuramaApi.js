1efa186f664e70ea33782e9f23fbb53a6b9ac7b9
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-08-16 14:44:27 -0700

    created a mock for testing

---
+++ src/services/__mocks__/futuramaApi.js
@@ -0,0 +1,9 @@
+export const getQuotes = () => {
+  return Promise.resolve([
+    {
+      "character": "Zapp Brannigan",
+      "quote": "I'm anticipating an all-out tactical dogfight, followed by a light dinner.",
+      "image": "https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904049/Zapp_Brannigan.png"
+    }
+  ]);
+};
